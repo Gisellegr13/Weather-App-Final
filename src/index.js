b29091bcaa118926d829d982b54d31f0d973d746
Giselle <58486290+Gisellegr92@users.noreply.github.com>
2022-07-08 17:13:44 -0400

    function display temp and description added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-function displayTemperature(response) {    
+function displayTemperature(response) {
     let temperatureElement = document.querySelector(".current-temperature");
-    let cityElement = document.querySelector(".city");
-    let descriptionElement = document.querySelector(".description")
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector(".description-weather");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
@@ -10,5 +10,4 @@ function displayTemperature(response) {
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Brandon&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
