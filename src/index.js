dc7f06ef65d7ccb89c22eaf631eb0c3cbeb8a8fa
Giselle <58486290+Gisellegr92@users.noreply.github.com>
2022-07-11 10:30:18 -0400

    Added relevant icon from the API

--- src/index.js
+++ src/index.js
@@ -1,29 +1,46 @@
-let date = new Date();
-
 function getMeridian(now) {
     return now.getHours() < 12 ? "am" : "pm";
 }
 
-function formatDate(){    
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     let day = days[date.getDay()];
     let month = date.getMonth();
     let dateNumber = date.getDate();
     let year = date.getFullYear();
-    return `${day} ${month + 1}-${dateNumber}-${year}`
+    return `  ${day} ${month + 1}-${dateNumber}-${year}`
 }
 
-function formatTime() {
+function formatTime(timestamp) {
     //calculate the date
-    let hours = date.getHours();
+    let time = new Date(timestamp)
+    let hours = time.getHours();
     if (hours < 10) {
         hours = `0${hours}`;
     }
-    let minutes = date.getMinutes();
+    if (hours > 12) {
+        hours = hours - 12;
+    }
+    let minutes = time.getMinutes();
     if (minutes < 10) {
         minutes = `0${minutes}`;
     }
-    let ampm = getMeridian(date);
+    // display minutes every 15 minutes
+    if (minutes < 15) {
+        minutes = "00";
+    }
+    if (minutes > 14 && minutes < 30) {
+        minutes = "15";
+    }
+    if (minutes > 29 && minutes < 45) {
+        minutes = "30";
+    }
+    if (minutes > 44 && minutes < 60) {
+        minutes = "45";
+    }
+
+    let ampm = getMeridian(time);
     return ` ${hours}:${minutes} ${ampm}`
 }
 
@@ -36,6 +53,8 @@ function displayTemperature(response) {
     let windElement = document.querySelector(".wind-span");
     let dateElement = document.querySelector(".date");
     let timeElement = document.querySelector(".hours");
+    let iconElement = document.querySelector("#icon-big");
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
@@ -43,9 +62,12 @@ function displayTemperature(response) {
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     timeElement.innerHTML = formatTime(response.data.dt * 1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description)
 }
 
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Brandon&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
